b6ebb95d7a13d75a6d520b1f91677e4aa74bdd39
sshilin <sv.shilin@gmail.com>
2019-12-06 15:38:14 +0300

    day5 cleanup

--- day5/main.go
+++ day5/main.go
@@ -8,170 +8,15 @@ import (
 
 type memory []int
 
-func (m memory) add(op1 int, op2 int, resPtr int) {
-	m[resPtr] = op1 + op2
+func (m memory) store(val int, addr int) {
+	m[addr] = val
 }
 
-func (m memory) mul(op1 int, op2 int, resPtr int) {
-	m[resPtr] = op1 * op2
-}
-
-func (m memory) load(ptr int) int {
-	return m[m[ptr]]
-}
-
-func (m memory) store(val int, ptr int) {
-	m[ptr] = val
-}
-
-func (m memory) run() {
-	ip := 0
-	halt := false
-	for !halt {
-		// fmt.Println("ip", ip, m[ip])
-		_, mop2, mop1, opcode := parseInstruction(m[ip])
-		ip++
-		// fmt.Println("Instruction:", mop3, mop2, mop1, opcode)
-		switch opcode {
-		case 1: // opcode 1 - add
-			op1, op2 := 0, 0
-			if mop1 == 1 {
-				op1 = m[ip]
-			} else {
-				op1 = m.load(ip)
-			}
-			if mop2 == 1 {
-				op2 = m[ip+1]
-			} else {
-				op2 = m.load(ip + 1)
-			}
-			m.store(op1+op2, m[ip+2])
-			ip += 3
-		case 2: // opcode 2 - mul
-			op1, op2 := 0, 0
-			if mop1 == 1 {
-				op1 = m[ip]
-			} else {
-				op1 = m.load(ip)
-			}
-			if mop2 == 1 {
-				op2 = m[ip+1]
-			} else {
-				op2 = m.load(ip + 1)
-			}
-			m.store(op1*op2, m[ip+2])
-			ip += 3
-		case 3: // opcode 3 - input
-			m.store(5, m[ip])
-			ip += 1
-		case 4: // opcode 3 - output
-			fmt.Println("=>", m[m[ip]])
-			ip += 1
-		case 5: // opcode 3 - jump-if-true
-			op1, op2 := 0, 0
-			if mop1 == 1 {
-				op1 = m[ip]
-			} else {
-				op1 = m.load(ip)
-			}
-			if mop2 == 1 {
-				op2 = m[ip+1]
-			} else {
-				op2 = m.load(ip + 1)
-			}
-
-			if op1 != 0 {
-				ip = op2
-			} else {
-				ip += 2
-			}
-		case 6: // opcode 3 - jump-if-false
-			op1, op2 := 0, 0
-			if mop1 == 1 {
-				op1 = m[ip]
-			} else {
-				op1 = m.load(ip)
-			}
-			if mop2 == 1 {
-				op2 = m[ip+1]
-			} else {
-				op2 = m.load(ip + 1)
-			}
-
-			if op1 == 0 {
-				ip = op2
-			} else {
-				ip += 2
-			}
-		case 7: // opcode 3 - less
-			op1, op2 := 0, 0
-			if mop1 == 1 {
-				op1 = m[ip]
-			} else {
-				op1 = m.load(ip)
-			}
-			if mop2 == 1 {
-				op2 = m[ip+1]
-			} else {
-				op2 = m.load(ip + 1)
-			}
-
-			if op1 < op2 {
-				m.store(1, m[ip+2])
-			} else {
-				m.store(0, m[ip+2])
-			}
-			ip += 3
-		case 8: // opcode 3 - equals
-			op1, op2 := 0, 0
-			if mop1 == 1 {
-				op1 = m[ip]
-			} else {
-				op1 = m.load(ip)
-			}
-			if mop2 == 1 {
-				op2 = m[ip+1]
-			} else {
-				op2 = m.load(ip + 1)
-			}
-
-			if op1 == op2 {
-				m.store(1, m[ip+2])
-			} else {
-				m.store(0, m[ip+2])
-			}
-			ip += 3
-
-		case 99: // opcode 99 - halt
-			halt = true
-		}
+func (m memory) load(mode int, addr int) int {
+	if mode == 1 {
+		return m[addr]
 	}
-}
-
-func part1(m memory) {
-	tmp := make(memory, len(m))
-	copy([]int(tmp), []int(m))
-	tmp[1] = 12
-	tmp[2] = 2
-	tmp.run()
-	fmt.Println("Part 1:", tmp[0]) // 2894520
-}
-
-func part2(m memory) {
-	for noun := 0; noun <= 99; noun++ {
-		for verb := 0; verb <= 99; verb++ {
-			tmp := make(memory, len(m))
-			copy([]int(tmp), []int(m))
-			tmp[1] = noun
-			tmp[2] = verb
-			tmp.run()
-			if tmp[0] == 19690720 {
-				fmt.Println("Part 2:", 100*noun+verb) // 9342
-				return
-			}
-		}
-	}
-	fmt.Println("not found")
+	return m[m[addr]]
 }
 
 func parseInstruction(instruction int) (mop3, mop2, mop1, opcode int) {
@@ -182,15 +27,73 @@ func parseInstruction(instruction int) (mop3, mop2, mop1, opcode int) {
 	return
 }
 
+func (m memory) run(intput int) {
+	ip := 0
+	halt := false
+	for !halt {
+		_, mop2, mop1, opcode := parseInstruction(m[ip])
+		ip++
+		switch opcode {
+		case 1: // add
+			m.store(m.load(mop1, ip)+m.load(mop2, ip+1), m[ip+2])
+			ip += 3
+		case 2: // mul
+			m.store(m.load(mop1, ip)*m.load(mop2, ip+1), m[ip+2])
+			ip += 3
+		case 3: // input
+			m.store(intput, m[ip])
+			ip++
+		case 4: // output
+			fmt.Println(m[m[ip]])
+			ip++
+		case 5: // jump-if-true
+			if m.load(mop1, ip) != 0 {
+				ip = m.load(mop2, ip+1)
+			} else {
+				ip += 2
+			}
+		case 6: // jump-if-false
+			if m.load(mop1, ip) == 0 {
+				ip = m.load(mop2, ip+1)
+			} else {
+				ip += 2
+			}
+		case 7: // less
+			if m.load(mop1, ip) < m.load(mop2, ip+1) {
+				m.store(1, m[ip+2])
+			} else {
+				m.store(0, m[ip+2])
+			}
+			ip += 3
+		case 8: // equals
+			if m.load(mop1, ip) == m.load(mop2, ip+1) {
+				m.store(1, m[ip+2])
+			} else {
+				m.store(0, m[ip+2])
+			}
+			ip += 3
+		case 99: // halt
+			halt = true
+		}
+	}
+}
+
+func part1(input []int) {
+	mem := make(memory, len(input))
+	copy([]int(mem), input)
+	mem.run(1) // 15508323
+}
+
+func part2(input []int) {
+	mem := make(memory, len(input))
+	copy([]int(mem), input)
+	mem.run(5) // 9006327
+}
+
 func main() {
 	if input, err := utils.ReadCSVInts("input.txt"); err == nil {
-		// part1(input)
-		// part2(input)
-		mem := make(memory, len(input))
-		copy([]int(mem), input)
-		// fmt.Println(mem)
-		mem.run() // part1: 15508323
-		// fmt.Println(mem)
+		part1(input)
+		part2(input)
 	} else {
 		fmt.Println(err)
 	}
